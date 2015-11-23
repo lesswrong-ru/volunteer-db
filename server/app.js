c5549b2c4c24cbe5bbd2d8ea0831fedb663057ba
Vyacheslav Matyukhin <mmcleric@yandex-team.ru>
2015-11-23 16:19:33 +0300

    snapshot

--- server/app.js
+++ server/app.js
@@ -3,6 +3,10 @@ import React from 'react';
 
 import Page from '../common/components/page';
 
+import sqlite3 from 'sqlite3';
+sqlite3.verbose();
+const db = new sqlite3.Database(':memory:');
+
 require("source-map-support").install(); // used by webpack-generated code
 
 const app = express();
@@ -15,6 +19,21 @@ app.get('/', (req, res) => {
       React.createElement(Page)
     )
   );
+});
+
+app.post('/api/register', (req, res) => {
+  const name = req.query.name;
+  if (!name) {
+    res.status(500).send({
+      error: 'expected name parameter',
+    });
+  }
+
+  db.run('CREATE TABLE IF NOT EXISTS users (name TEXT)');
+  db.run('INSERT users SET name = ?', name);
+  res.send({
+    status: 'ok',
+  });
 });
 
 const server = app.listen(8080);
